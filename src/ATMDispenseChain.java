2ffab9b173917ec75b72f431dfe5e4f681fdd8d9
MaxiAdaro <maxiadaro1999@gmail.com>
2020-10-26 21:51:22 -0300

    remove unnecessary value

--- src/ATMDispenseChain.java
+++ src/ATMDispenseChain.java
@@ -18,7 +18,7 @@ public class ATMDispenseChain {
   public static void main(String[] args) {
     ATMDispenseChain atmDispenser = new ATMDispenseChain();
     while (true) {
-      int amount = 0;
+      int amount;
       System.out.println("Enter amount to dispense");
       Scanner input = new Scanner(System.in);
       amount = input.nextInt();
